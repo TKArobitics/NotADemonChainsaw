e946a2be96ee519829d84241029a5c08242c23bc
Your Name <you@example.com>
2023-04-13 20:50:19 -0400

    EVERYTHING

--- src/blueShort.cpp
+++ src/blueShort.cpp
@@ -22,8 +22,8 @@ using namespace vex;
 
 void blueShort(){
   Brain.Screen.print("Running blueShort.\n");
-  startLauncher(80);
-  autoConveyor(25);
+  // startLauncher(80);
+  // autoConveyor(25);
   setMotors(10);
   allForward(50);
   setMotors(50);
@@ -32,7 +32,7 @@ void blueShort(){
   mechTranslate(375);
   allForward(-250);
   rightSideOnly(-350); 
-  setMotors(10);
+  setMotors(5);
   allForwardc(-80);
   turnRoller(-600);
   wait(2, sec);
@@ -47,3 +47,35 @@ void blueShort(){
 
   //next time adjust the angle when doing the roller by only driving the right side.
 }
+
+void autonomousSkills(){
+  setMotors(10);
+  allForwardc(-100);
+  turnRollerTrue(600);
+  setMotors(50);
+  allForward(300);
+  tankTurn(-300);
+  allForward(-300);
+  setMotors(10);
+  allForwardc(100000);
+  turnRollerTrue(600);
+  setMotors(50);
+  allForward(-100);
+  tankTurn(300);
+  allForward(600);
+  mechTranslate(600);
+
+}
+
+void skillsDisksFirst(){
+  spinUp();
+  wait(7, sec);
+  autoConveyor();
+  wait(30, sec);
+  conveyorStop();
+  flyWheelStop();
+  tankTurn(300);
+  expandSkill();
+
+
+} 
